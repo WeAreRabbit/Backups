5655d3de832d69444c70b18c5c33ece10712a322
WeAreRabbit <128472598+WeAreRabbit@users.noreply.github.com>
2023-04-14 02:14:44 +0700

    Create HttpHandler.lua

---
+++ MaruHub/HttpHandler.lua
@@ -0,0 +1,28 @@
+-- Blox Fruit Assets
+-- https://raw.githubusercontent.com/xshiba/Themes/main/load.lua : Theme
+-- https://raw.githubusercontent.com/xshiba/LoadNew/main/LoadA.lua : Aimbot
+-- https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua : Loader
+
+-- King Legacy Assets
+-- https://raw.githubusercontent.com/xshiba/AimYa/main/Load.lua : Aimbot
+-- https://raw.githubusercontent.com/xshiba/LoveCream/main/LOADLK.lua : Loader
+
+-- Last Pirate
+-- https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadAll.lua : Loader
+
+-- https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua : Main Loader
+
+-- HttpGet Handle
+local HttpGet, meta__namecall;
+HttpGet = hookfunction(game.HttpGet, function(self, url, ...)
+    if url == "https://raw.githubusercontent.com/xshiba/Themes/main/load.lua" then
+        return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\84\104\101\109\101\46\108\117\97", ...)
+    elseif url == "https://raw.githubusercontent.com/xshiba/LoadNew/main/LoadA.lua" then
+        return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\66\70\77\101\116\97\46\108\117\97", ...)
+    elseif url == "https://raw.githubusercontent.com/xshiba/AimYa/main/Load.lua" then
+        return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\75\76\77\101\116\97\46\108\117\97", ...)
+    else
+        print(url)
+    end
+    return HttpGet(self, url, ...)
+end);
