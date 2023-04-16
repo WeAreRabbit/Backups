1d4bf68627901434a37cff7b632a327b9b58f183
WeAreRabbit <128472598+WeAreRabbit@users.noreply.github.com>
2023-04-16 23:15:27 +0700

    Update HttpHandler.lua

--- MaruHub/HttpHandler.lua
+++ MaruHub/HttpHandler.lua
@@ -3,6 +3,10 @@
 -- https://raw.githubusercontent.com/xshiba/LoadNew/main/LoadA.lua : Aimbot
 -- https://raw.githubusercontent.com/xshiba/LoveCream/main/LoadMain.lua : Loader
 
+-- Blox Fruit V2
+-- https://raw.githubusercontent.com/xshiba/OBFUSCATE/main/LURAPH-1.lua : UI Library
+-- https://raw.githubusercontent.com/xshiba/APIMETAABLE/main/LOAD.lua : Metatable Hook
+
 -- King Legacy Assets
 -- https://raw.githubusercontent.com/xshiba/AimYa/main/Load.lua : Aimbot
 -- https://raw.githubusercontent.com/xshiba/LoveCream/main/LOADLK.lua : Loader
@@ -21,8 +25,10 @@ HttpGet = hookfunction(game.HttpGet, function(self, url, ...)
         return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\66\70\77\101\116\97\46\108\117\97", ...)
     elseif url == "https://raw.githubusercontent.com/xshiba/AimYa/main/Load.lua" then
         return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\75\76\77\101\116\97\46\108\117\97", ...)
-    else
-        print(url)
+    elseif url == "https://raw.githubusercontent.com/xshiba/OBFUSCATE/main/LURAPH-1.lua" then
+        return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\76\105\98\114\97\114\121\46\108\117\97", ...)
+    elseif url == "https://raw.githubusercontent.com/xshiba/APIMETAABLE/main/LOAD.lua" then
+        return HttpGet(self, "\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\87\101\65\114\101\82\97\98\98\105\116\47\66\97\99\107\117\112\115\47\109\97\105\110\47\77\97\114\117\72\117\98\47\65\80\73\77\69\84\65\65\66\76\69\46\108\117\97", ...)
     end
     return HttpGet(self, url, ...)
 end);
